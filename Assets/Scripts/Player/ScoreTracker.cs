3e8c286fb908b441d7f1f1460ab9c6c9493d6bd2
wiwechen <129801256+wiwechen@users.noreply.github.com>
2023-10-29 16:53:50 -0700

    UI Collectable is finished

--- Assets/Scripts/Player/ScoreTracker.cs
+++ Assets/Scripts/Player/ScoreTracker.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class ScoreTracker : MonoBehaviour
 {
     [SerializeField] static private int numCollectibles = 0;
     [SerializeField] public int totalCollectibles = 5;
+    public TMP_Text text;
+
     // Start is called before the first frame update
     void Start()
     {
         numCollectibles = 0;
+        text.text = "Collectable: " + numCollectibles;
     }
 
     // Update is called once per frame
@@ -21,8 +25,9 @@ public class ScoreTracker : MonoBehaviour
 
     public void collectibleObtained(){
         numCollectibles++;
-        //Debug.Log("Collected: " + numCollectibles);
-        if(numCollectibles >= totalCollectibles){
+        Debug.Log("Collected: " + numCollectibles);
+        text.text = "Collectable: " + numCollectibles;
+        if (numCollectibles >= totalCollectibles){
             SceneManager.LoadScene("Win");
         }
     }
